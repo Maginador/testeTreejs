f94b4d0fc49c31b3ec5ee037f17d7b1652a38585
Maginador <maginador@gmail.com>
2023-11-27 18:19:43 -0300

    add new structure to reference-mapper and make the mapping work for video texture and emission (sample using TV name)

--- mapper.js
+++ mapper.js
@@ -1,29 +1,71 @@
-function GetMapperJson(){
+async function GetMapperJson(){
 
-    fetch("./reference-mapper.json") 
-    .then((res) => { 
-    return res.json(); 
-}) 
-.then((data) => console.log(data)); 
+    const response = await fetch("./reference-mapper.json"); 
+    const json = await response.json();
+    return json;
+}
+function BuildLight(json){
+
+    let lightsRawData = json.lights;
+
+lightsRawData.forEach(element => {
+    let light;
+    switch(element.type){
+        case "point": 
+        default :
+            light = new THREE.PointLight(element.color);
+            break;
+        case "spot" : 
+            light = new THREE.SpotLight(element.color);
+            break;
+        case "hemisphere":
+            light = new THREE.HemisphereLight(element.color);
+            break;
+        
+    }
+    //TODO Verify if it is possible to store the data directly from exporter
+    light.position.set(element.position.x,element.position.y,element.position.z)
+    light.intensity = element.intensity;
+});
 }
 
-function BuildMaterials(){
-//TODO Build List of Materials jsonData.materials[];
+async function MapMaterial(src){
+
+    let mat = BuildDefaultMaterial();
+    for(let i = 0; i<jsonMaterials.length; i++){
+        if(src.startsWith(jsonMaterials[i].pattern.prefix) || 
+        src.endsWith(jsonMaterials[i].pattern.sufix) || 
+        src.includes(jsonMaterials[i].pattern.contains) ||
+        src === jsonMaterials[i].pattern.equals){
+            mat = await BuildMaterial(jsonMaterials[i]);
+        }  
+    }
+    return mat;
+
 }
 
-function BuildLights(){
-//TODO Build List of Lights jsonData.lights[];
+function BuildDefaultMaterial(){
+    const mat =  new THREE.MeshStandardMaterial();
+    //in case of any specification for default materials, it can be added here
 
+    return mat;
 }
-
-function MapMaterial(src){
-
-//TODO return material based on json specifications jsonData.models[].pattern
-
-//TODO return material if pattern is found jsonData.models[].material-name 
+async function BuildMaterial(material){
+    let mat = new THREE.MeshStandardMaterial();
+    mat.emissive = new THREE.Color(material.emissioncolor);
+    mat.emissiveIntensity = parseFloat(material.emissionintensity);
+    if(material.videotexture){
+        let video = document.getElementById( 'video' );
+        video.src = "./" + material.videosource;
+        const texture = new THREE.VideoTexture( video );
+        texture.colorSpace = THREE.SRGBColorSpace;
+        mat.map = texture;
+    }
+    return mat; 
 }
-let jsonData = GetMapperJson();
-
-//TODO List Material Templates 
-
-//
+let jsonData;
+let jsonMaterials;
+async function InitMapper(){
+    jsonData = await GetMapperJson();
+    jsonMaterials = jsonData.materials;    
+}
