e8e64c623d464f10abf9fecde0e167eb5feba869
Maginador <maginador@gmail.com>
2023-12-10 17:45:23 -0300

    final implementation of ruler feature

--- ruler.js
+++ ruler.js
@@ -1,172 +1,262 @@
 import * as THREE from 'three';
-import { TextGeometry } from '/jsm/geometries/TextGeometry.js';
-import { FontLoader } from '/jsm/loaders/FontLoader.js';
 
+let boxClose;
 let vertexList = [];
 let linesList = [];
 let metricsList = [];
+let measureListElement;
+let closeListElement;
+let measureList = [];
+let closeList = [];
+let markers = [];
+
 const markerSize = 2;
 const markerHsegments = 12;
 const markerWsegments = 12;
 
 let refSphere;
-function PickRulerPoint(objects){
+
+function PickRulerPoint(objects) {
 
     let dist = 99999;
     let index = -1;
-    for(let i =0; i<objects.length; i++){
-        if(dist > objects[i].distance){
+    for (let i = 0; i < objects.length; i++) {
+        if (dist > objects[i].distance) {
             dist = objects[i].distance;
             index = i;
-        }   
-    }    
+        }
+    }
 
     //constants 
-    if(!objects[index])
+    if (!objects[index])
         return null;
     else {
         return objects[index].point;
     }
-        
-   
+
+
 }
 
-function AddLine(v1, v2){ 
+function AddLine(v1, v2) {
 
-    const material = new THREE.LineBasicMaterial( { color: 0xff00 } );
+    const material = new THREE.LineBasicMaterial({ color: 0xff00 });
     const points = [];
     points.push(v1);
     points.push(v2);
-    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
     material.depthTest = false;
-    const line = new THREE.Line( geometry, material );
+    const line = new THREE.Line(geometry, material);
     line.renderOrder = 1;
     scene.add(line);
     return line;
 }
 
-function HideSphere(){
-    if(refSphere) refSphere.visible = false;
+function HideSphere() {
+    if (refSphere) refSphere.visible = false;
+
 }
 
-function MovePoint(point){
-    if(!refSphere){
-        const geometry = new THREE.SphereGeometry( markerSize, markerHsegments, markerWsegments ); 
-        const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } ); 
+function MovePoint(point) {
+    if(metricsList.length >= 8) return;
+    if (!refSphere) {
+        const geometry = new THREE.SphereGeometry(markerSize, markerHsegments, markerWsegments);
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
         material.depthTest = false;
-        const sphere = new THREE.Mesh( geometry, material ); 
-    
-        sphere.position.set(point.x+(markerSize/2), point.y+(markerSize/2), point.z+(markerSize/2));
+        const sphere = new THREE.Mesh(geometry, material);
+
+        sphere.position.set(point.x + (markerSize / 2), point.y + (markerSize / 2), point.z + (markerSize / 2));
         sphere.renderOrder = 1;
 
-        scene.add( sphere );
+        scene.add(sphere);
         refSphere = sphere;
     }
 
     refSphere.visible = true;
-    refSphere.position.set(point.x,point.y, point.z);
+    refSphere.position.set(point.x, point.y, point.z);
 }
-function AddPoint(point){
-    
-    
-    const geometry = new THREE.SphereGeometry( markerSize, markerHsegments, markerWsegments ); 
-    const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
+function AddPoint(point) {
+
+
+    const geometry = new THREE.SphereGeometry(markerSize, markerHsegments, markerWsegments);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
     material.depthTest = false;
-    const sphere = new THREE.Mesh( geometry, material ); 
-    
-    sphere.position.set(point.x+(markerSize/2), point.y+(markerSize/2), point.z+(markerSize/2));
+    const sphere = new THREE.Mesh(geometry, material);
+
+    sphere.position.set(point.x + (markerSize / 2), point.y + (markerSize / 2), point.z + (markerSize / 2));
     sphere.renderOrder = 1;
 
-    scene.add( sphere );
+    scene.add(sphere);
     vertexList.push(sphere.position);
-    
-}
-
-function AddMetrics(p1,p2){
-    const loader = new FontLoader();
-
-loader.load( 'fonts/Arial_Regular.json', function ( font ) {
-
-    const material = new THREE.MeshPhongMaterial( { color: 0xffff00 } ); 
-    material.depthTest = false;
-    var dist = p1.distanceTo(p2);
-	const geometry = new TextGeometry( dist.toString(), {
-		font: font,
-		size: 20,
-		height: 5,
-		curveSegments: 30,
-		bevelEnabled: true,
-		bevelThickness: 4,
-		bevelSize: 8,
-		bevelOffset: 0,
-		bevelSegments: 5
-	} );
-    const text = new THREE.Mesh( geometry, material ); 
-    text.position.set (p1.x, p1.y, p1.z);
-    text.renderOrder = 1;
-
-    scene.add(text);
-
-} );
+    markers.push(sphere);
 
 }
 
-window.RulerRaycast = function RulerRaycast(camera, scene){
+window.RulerRaycast = function RulerRaycast(camera, scene) {
 
-    raycaster.setFromCamera( pointer, camera );
-	// calculate objects intersecting the picking ray
-	const intersects = raycaster.intersectObjects( window.sceneObjects);
+    raycaster.setFromCamera(pointer, camera);
+    // calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects(window.sceneObjects);
 
     const point = PickRulerPoint(intersects);
-    if(!point) {
+    if (!point) {
         HideSphere();
         return;
     }
     MovePoint(point);
-    
-    
-    if(vertexList.length !== 0 && vertexList.length%2 == 0){
-        const v1 = vertexList[vertexList.length-1];
-        const v2 = vertexList[vertexList.length-2];
-        linesList.push(AddLine(v1, v2));
-        //metricsList.push(AddMetrics(v1, v2));
-        rulerAddPoint = false;
-        HideSphere();
-    }
+
+
+
 
 }
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 const clickPointer = new THREE.Vector2();
 const limitThreshold = 0.1;
-function onPointerMove( event ) {
+function onPointerMove(event) {
 
-	// calculate pointer position in normalized device coordinates
-	// (-1 to +1) for both components
+    // calculate pointer position in normalized device coordinates
+    // (-1 to +1) for both components
 
-	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
 }
 
-function OnMouseDown(event){
+function UpdateFields() {
+    if (!measureListElement) {
+        measureListElement = document.getElementById("measureList");
+    }
+    if(!closeListElement){
+        closeListElement = document.getElementById("closeList");
+    }
+    var diff = metricsList.length - measureList.length;
+    if (diff > 0) {
+        for (let i = 0; i < diff; i++) {
+            var element = document.createElement("div");
+            var trash = document.createElement("div");
+            trash.textContent = "X";
+            //element.appendChild(trash);
+            measureListElement.appendChild(element);
+            closeListElement.appendChild(trash);
+
+            measureList.push(element);
+            closeList.push(trash);
+        }
+    }
+    for (let i = 0; i < metricsList.length; i++) {
+        measureList[i].textContent = metricsList[i].distance.toFixed(2) + 'cm';
+        measureList[i].index = i;
+        measureList[i].addEventListener("mouseover",onMeasureMouseOver);
+        measureList[i].addEventListener("mouseout",onMeasureMouseOut);
+        closeList[i].index = i;
+        closeList[i].id = "X"+i;
+        closeList[i].addEventListener("click",onCloseMeasureButton)
+    }
+}
+function RemoveElement(index){
+   closeList[index].remove();
+   measureList[index].remove();
+   scene.remove(metricsList[index].marker1);
+   scene.remove(metricsList[index].marker2);
+   scene.remove(linesList[index]);
+   delete closeList[index];
+   delete measureList[index];
+   delete metricsList[index];
+   var newCloseArray = [];
+   var newMeasureArray = [];
+    var newMetricsArray = [];
+    for(let i =0; i < closeList.length; i++){
+        if(closeList[i]){
+            newCloseArray.push(closeList[i]);
+        }
+        if(measureList[i]){
+            newMeasureArray.push(measureList[i]);
+        }
+        if(metricsList[i]){
+            newMetricsArray.push(metricsList[i]);
+        }
+    }
+    closeList = newCloseArray;
+    measureList = newMeasureArray;
+    metricsList = newMetricsArray;
+
+    UpdateFields();
+}
+function ResetHighlight(){
+    for(let i = 0; i<metricsList.length; i++){
+
+        metricsList[i].marker1.material.color = new THREE.Color(0xffffff);
+        metricsList[i].marker2.material.color = new THREE.Color(0xffffff);
+        metricsList[i].marker1.scale.set(1,1,1);
+        metricsList[i].marker2.scale.set(1,1,1);
+   }
+}
+function HighlightMarkers(index){
+   for(let i = 0; i<metricsList.length; i++){
+
+        metricsList[i].marker1.material.color = new THREE.Color(0xffffff);
+        metricsList[i].marker2.material.color = new THREE.Color(0xffffff);
+        metricsList[i].marker1.scale.set(1,1,1);
+        metricsList[i].marker2.scale.set(1,1,1);
+
+
+   }
+   const highlightScale = 3;
+   const highlightColor = 0x00ff00;
+   metricsList[index].marker1.material.color = new THREE.Color(highlightColor);
+   metricsList[index].marker2.material.color = new THREE.Color(highlightColor);
+   metricsList[index].marker1.scale.set(highlightScale,highlightScale,highlightScale);
+   metricsList[index].marker2.scale.set(highlightScale,highlightScale,highlightScale);
+
+}
+function onCloseMeasureButton(event){
+    var index = event.currentTarget.index;
+    RemoveElement(index);
+}
+function onMeasureMouseOut(event){
+    ResetHighlight(); 
+}
+function onMeasureMouseOver(event){
+   var index = event.currentTarget.index;
+   HighlightMarkers(index);
+}
+
+function OnMouseDown(event) {
     //saves pointer on click
     clickPointer.x = pointer.x;
     clickPointer.y = pointer.y;
-    console.log("mouse down");
 }
 
-function OnMouseUp(event){
-    console.log("mouse up");
-    console.log(clickPointer.distanceTo(pointer));
-    console.log(clickPointer);
-    console.log(pointer);
+function onClick(event) {
     //check if pointer moved less than the threshold
-    if(clickPointer && clickPointer.distanceTo(pointer) < limitThreshold){ 
-        if(refSphere)AddPoint(refSphere.position);
+    if (clickPointer && clickPointer.distanceTo(pointer) < limitThreshold) {
+        if (refSphere && refSphere.visible) {
+            AddPoint(refSphere.position);
+            if (vertexList.length !== 0 && vertexList.length % 2 == 0) {
+                const v1 = vertexList[vertexList.length - 1];
+                const v2 = vertexList[vertexList.length - 2];
+
+                const m1= markers[markers.length - 1];
+                const m2= markers[markers.length - 2];
+                linesList.push(AddLine(v1, v2));
+                metricsList.push({ distance: v1.distanceTo(v2), point1: v1, marker1:m1, point2: v2, marker2:m2 });
+                UpdateFields();
+                HideSphere();
+
+            }
+        }
     }
 }
-window.addEventListener( 'pointermove', onPointerMove );
-window.addEventListener( 'click', OnMouseUp );
-window.addEventListener( 'mousedown', OnMouseDown );
+
+function onExitClick(event) {
+    rulerAddPoint = false;
+    window.boxBase.style.display = 'none';
+
+}
+window.addEventListener('pointermove', onPointerMove);
+window.addEventListener('click', onClick);
+window.addEventListener('mousedown', OnMouseDown);
+window.boxBase = document.getElementById("boxBase");
+boxClose = document.getElementById("boxClose");
+boxClose.addEventListener('click', onExitClick);
 
